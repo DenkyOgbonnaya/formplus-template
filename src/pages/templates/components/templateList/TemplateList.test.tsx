4fbaadc107429b5258013cb5afd049565e87c891
DenkyOgbonnaya <denkyogb@gmail.com>
2022-02-10 05:04:05 +0100

    test:write test for template list

---
+++ src/pages/templates/components/templateList/TemplateList.test.tsx
@@ -0,0 +1,35 @@
+import { render } from "@testing-library/react";
+import { ITemplate } from "sharable/interface";
+import TemplateList from "./TemplateList";
+
+describe("<Template />", () => {
+  let templates: ITemplate[];
+  beforeEach(() => {
+    templates = [
+      {
+        name: "Test",
+        description: "Short description",
+        link: "",
+        category: ["Health"],
+        created: "",
+      },
+      {
+        name: "Test",
+        description: "Short description",
+        link: "",
+        category: ["Health"],
+        created: "",
+      },
+    ];
+  });
+  it("Should show the correct number of templates", () => {
+    const mockFn = jest.fn();
+    const { getAllByText } = render(
+      <TemplateList templates={templates} useHandler={mockFn} />
+    );
+
+    const templateNames = getAllByText(templates[0].name);
+
+    expect(templateNames.length).toBe(2);
+  });
+});
